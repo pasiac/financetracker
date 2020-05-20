0ce626646c00a031a2562806972cb657c6ae8830
dawidpawlowski <pawlowskidawid@pm.me>
2020-05-21 00:54:01 +0200

    wywalone komenatrze z settings

--- settings.py
+++ settings.py
@@ -2,7 +2,6 @@ import os
 
 from django.conf.global_settings import STATIC_ROOT
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 SECRET_KEY = "=y#15_$8u9mm_iozl8b+*+@1lao-l&xy!rn9($xcy9^*zwgt23"
@@ -10,8 +9,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
 
 INSTALLED_APPS = [
     "tracker.apps.TrackerConfig",
@@ -54,9 +51,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -64,9 +58,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -78,22 +69,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "media/static/"
 STATIC_ROOT = "media/static/"
