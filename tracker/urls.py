bfa62499352f010f0ffd16dcb46f92911fe1bac9
dawidpawlowski <pawlowskidawid@pm.me>
2020-07-06 22:41:11 +0200

    json for expanses/date chart

--- tracker/urls.py
+++ tracker/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     path("edytuj-kategorie/<category_id>", views.edit_category, name="edit_category"),
     path("usun-kategorie/<category_id>", views.delete_category, name="delete_category"),
     path("generate-csv", views.generate_csv, name="generate_csv"),
+    path("charts", views.chart_tab, name="chart-tab"),
     # path("wykresy", views.charts, name="charts"),
 ]
