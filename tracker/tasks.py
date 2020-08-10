98ca37dce8da7571fdff62928d9c2bc227c32523
dawidpawlowski <pawlowskidawid@pm.me>
2020-08-11 00:23:41 +0200

    Scraping auchan site with requests and bs4

--- tracker/tasks.py
+++ tracker/tasks.py
@@ -1,8 +1,14 @@
 from __future__ import absolute_import, unicode_literals
 
 from celery import task
+from .models import IncomeOutcome
+import requests
+from bs4 import BeautifulSoup
 
 
 @task()
 def get_prices():
-    print("hello world")
+    items_names = IncomeOutcome.objects.values_list("title", flat=True)
+    items_response_list = [requests.get(f'https://zakupy.auchan.pl/shop/search/{name}') for name in items_names]
+    
+    print("I have to use silenium to complete this task")
