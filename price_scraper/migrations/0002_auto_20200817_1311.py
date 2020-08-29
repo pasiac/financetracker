f1d73500eb94e431c3eae1b3380562f0757c37de
dawidpawlowski <pawlowskidawid@pm.me>
2020-08-29 16:17:11 +0200

    First step of code refactoring

--- price_scraper/migrations/0002_auto_20200817_1311.py
+++ price_scraper/migrations/0002_auto_20200817_1311.py
@@ -11,6 +11,8 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AlterField(
-            model_name="product", name="title", field=models.CharField(max_length=128),
+            model_name="product",
+            name="title",
+            field=models.CharField(max_length=128),
         ),
     ]
--- settings.py
+++ settings.py
@@ -1,8 +1,5 @@
 import os
 
-from celery.schedules import crontab
-from django.conf.global_settings import STATIC_ROOT
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 SECRET_KEY = "=y#15_$8u9mm_iozl8b+*+@1lao-l&xy!rn9($xcy9^*zwgt23"
@@ -40,7 +37,9 @@ ROOT_URLCONF = "urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [(BASE_DIR + "/templates/"),],
+        "DIRS": [
+            (BASE_DIR + "/templates/"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -68,9 +67,15 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
 
@@ -82,7 +87,6 @@ USE_TZ = True
 
 
 STATIC_URL = "media/static/"
-STATIC_ROOT = ""
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
