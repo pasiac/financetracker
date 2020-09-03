2300ae13ba922b3a47a9cf117893f065b8f6e751
dawidpawlowski <pawlowskidawid@pm.me>
2020-09-04 00:05:05 +0200

    Delete not nessesery files

--- price_scraper/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from price_scraper.models import Price, Product
-
-admin.site.register(Price)
-admin.site.register(Product)
--- price_scraper/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PriceScraperConfig(AppConfig):
-    name = "price_scraper"
