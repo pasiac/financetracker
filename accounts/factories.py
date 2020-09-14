6f4d10caf16de9a772b8401dcd29f72dda7f2f40
dawidpawlowski <pawlowskidawid@pm.me>
2020-09-14 20:35:24 +0200

    First changes, black on whole project

--- accounts/factories.py
+++ accounts/factories.py
@@ -1,12 +1,11 @@
 from factory.django import DjangoModelFactory
 import factory
-from factory.fuzzy import FuzzyText
 from django.contrib.auth.models import User
 
+
 class UserFactory(DjangoModelFactory):
-    username = factory.Sequence('testuser{}'.format)
-    email = factory.Sequence('testuser{}@company.com'.format)
-    
+    username = factory.Sequence("testuser{}".format)
+    email = factory.Sequence("testuser{}@company.com".format)
 
     class Meta:
         model = User
