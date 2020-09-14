bab0aa39710cb4dc6b328257af96010cb310e836
dawidpawlowski <pawlowskidawid@pm.me>
2020-09-14 21:36:59 +0200

    Templates

--- expense/urls.py
+++ expense/urls.py
@@ -5,8 +5,8 @@ from expense.views import (ExpenseCreateView, ExpenseDeleteView,
 
 urlpatterns = [
     path("", ExpenseListView.as_view(), name="expense_list"),
-    path("<int:pk>/", ExpenseDetailView.as_view(), name="expanse_detail"),
-    path("usun/<int:pk>/", ExpenseDeleteView.as_view(), name="delete_expanse"),
-    path("edytuj/<int:pk>/", ExpenseEditView.as_view(), name="edit_expanse"),
-    path("dodaj/", ExpenseCreateView.as_view(), name="add_expanse"),
+    path("<int:pk>/", ExpenseDetailView.as_view(), name="expense_detail"),
+    path("usun/<int:pk>/", ExpenseDeleteView.as_view(), name="delete_expense"),
+    path("edytuj/<int:pk>/", ExpenseEditView.as_view(), name="edit_expense"),
+    path("dodaj/", ExpenseCreateView.as_view(), name="add_expense"),
 ]
