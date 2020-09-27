e1ba5649e2b3f6d4437d5546f3cfe498f85f196c
dpawlowski <dpawlowski@grupazpr.pl>
2020-09-27 16:22:58 +0200

    Some fixes

--- categories/models.py
+++ categories/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=30)
-    created_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     def get_related_expenses_value_list(self):
         return self.expense_set.values_list("value", flat=True)
--- price_scraper/views.py
+++ price_scraper/views.py
@@ -3,7 +3,6 @@ from django.views.generic.list import ListView
 from price_scraper.models import Price
 
 
-# Create your views here.
 class PriceListView(ListView):
     model = Price
     paginate_by = 20
