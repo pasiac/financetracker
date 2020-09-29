0789aae0316f4333b224e9b1cc55e1742c29d0c8
dawid <dawid@grupazpr.pl>
2020-09-29 23:32:58 +0200

    Filters no blank lines

--- expense/filters.py
+++ expense/filters.py
@@ -6,9 +6,7 @@ from expense.models import Expense
 class ExpenseFilter(django_filters.FilterSet):
     value__gt = django_filters.NumberFilter(field_name="value", lookup_expr="gt")
     value__lt = django_filters.NumberFilter(field_name="value", lookup_expr="lt")
-
     date = django_filters.DateRangeFilter(field_name="date", lookup_expr="date__lte")
-
     category = django_filters.ChoiceFilter()
 
     class Meta:
